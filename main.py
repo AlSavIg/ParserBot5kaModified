489994bbca102588b190e6ef14a1be75b806db9a
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-06-03 00:12:27 +0300

    Нужно поправить parse.py - проблемы с русским языком

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-import parse
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+from aiofiles import os
+from parse import collect_data
 
-if __name__ == "__main__":
-    print("Hello")
+token = "5387713157:AAHc0bfR2F8m1WzPiCfPa4_ACi0K39VCZV4"
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    start_buttons = ['г.Санкт-Петербург, Наставников пр, 3', 'г.Санкт-Петербург, пр-кт Косыгина, 31']
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+    await message.answer('Пожалуйста, выберите магазин', reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals='г.Санкт-Петербург, Наставников пр, 3'))
+async def nastavnikov_street(message: types.Message):
+    await message.answer('Ожидайте...')
+    chat_id = message.chat.id
+    await send_data(shop_id='7215', chat_id=chat_id)
+
+
+@dp.message_handler(Text(equals='г.Санкт-Петербург, пр-кт Косыгина, 31'))
+async def kosigina_street(message: types.Message):
+    await message.answer('Ожидайте...')
+    chat_id = message.chat.id
+    await send_data(shop_id='28870', chat_id=chat_id)
+
+
+async def send_data(shop_id, chat_id):
+    file = await collect_data(shop_id=shop_id)
+    await bot.send_document(chat_id=chat_id, document=open(file, 'rb'))
+    await os.remove(file)
+
+
+def main():
+    executor.start_polling(dp)
+
+
+if __name__ == '__main__':
+    main()
--- parse_regular.py
+++ parse_regular.py
@@ -1,4 +1,6 @@
 import datetime
+
+import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import aiohttp
@@ -40,10 +42,10 @@ async def collect_data(shop_id):
     }
 
     cookies = {
-    #     'selectedStore': f'{selected_store[shop_id]}'
-    # }
-        'selectedStore': '%7B%22id%22%3A28870%2C%22sap_code%22%3A%2233YU%22%2C%22address%22%3A%22%D0%B3.%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%BF%D1%80-%D0%BA%D1%82%20%D0%9A%D0%BE%D1%81%D1%8B%D0%B3%D0%B8%D0%BD%D0%B0%2C%2031%22%2C%22work_start_time%22%3A%2208%3A00%3A00%22%2C%22work_end_time%22%3A%2223%3A00%3A00%22%2C%22is_24h%22%3Afalse%7D"'
+        'selectedStore': f'{selected_store[shop_id]}'
     }
+
+    print('LOADING')
 
     async with aiohttp.ClientSession() as session:
 
@@ -77,14 +79,13 @@ async def collect_data(shop_id):
     async with aiofiles.open(f'{selected_store_names[shop_id]}_{cur_time}.csv', 'w') as file:
         writer = AsyncWriter(file)
 
-        print('LOADING')
         await writer.writerow(
             [
-                'Product',
-                'New cost',
-                'Old cost',
-                'Discount (percent)',
-                'Discount time',
+                'Продукт',
+                'Новая цена',
+                'Старая цена',
+                'Процент скидки',
+                'Время акции',
             ]
         )
         await writer.writerows(data)
@@ -96,5 +97,52 @@ async def main():
     await collect_data(shop_id='7215')
 
 
+def test(shop_id='7215'):
+    ua = UserAgent()
+
+    selected_store = {
+        '28870':
+            '{"id": 28870,'
+            '"sap_code": "33YU",'
+            '"address": "г.Санкт-Петербург, пр-кт Косыгина, 31",'
+            '"work_start_time": "08:00:00",'
+            '"work_end_time": "23:00:00",'
+            '"is_24h": false}',
+        '7215':
+            '{"id": 7215,'
+            '"sap_code": "5677",'
+            '"address": "г.Санкт-Петербург, Наставников пр, 3",'
+            '"work_start_time": "09:00:00",'
+            '"work_end_time": "23:00:00",'
+            '"is_24h": false}'
+    }
+
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                  'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': ua.random
+    }
+
+    cookies = {
+        # '_gcl_au': '1.1.798722131.1651174421; ',
+        # '_ym_d': '1651174421; ',
+        # '_ym_uid': '1651174421723545144; ',
+        # '_ym_isad': '1; ',
+        # '_gid': 'GA1.2.1639026830.1654177152; ',
+        # 'location': '{"id":12147,"name":"г.Санкт-Петербург","type":"city","new_loyalty_program":true,"site_shops_count":416,"region":{"id":30,"name":"Санкт-Петербург"}}; ',
+        # 'location_id': '12147;',
+        # 'selectedStore': '{"id":28870,"sap_code":"33YU","address":"г.Санкт-Петербург, пр-кт Косыгина, 31","work_start_time":"08:00:00","work_end_time":"23:00:00","is_24h":false}; ',
+        # # 'selectedStore': f'{selected_store[shop_id]}'
+
+        'selectedStore': '%7B%22id%22%3A28870%2C%22sap_code%22%3A%2233YU%22%2C%22address%22%3A%22%D0%B3.%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%BF%D1%80-%D0%BA%D1%82%20%D0%9A%D0%BE%D1%81%D1%8B%D0%B3%D0%B8%D0%BD%D0%B0%2C%2031%22%2C%22work_start_time%22%3A%2208%3A00%3A00%22%2C%22work_end_time%22%3A%2223%3A00%3A00%22%2C%22is_24h%22%3Afalse%7D"'
+    }
+
+    response = requests.get(url='https://5ka.ru/special_offers', headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.text, 'lxml')
+    with open('index.html', 'w') as file:
+        print(response.status_code)
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    # asyncio.run(main())
+    test()
