661d2778442fe00efe88df12d83fe1cf8a605d32
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-06-04 00:49:48 +0300

    Рабочая версия программы для парсинга начальной страницы. Необходимо расширить функционал (парсинг динамических страниц)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 from aiofiles import os
-from parse import collect_data
+from parse import collect_data, selected_store_names
 
 token = "5387713157:AAHc0bfR2F8m1WzPiCfPa4_ACi0K39VCZV4"
 bot = Bot(token=token)
@@ -10,30 +10,37 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    start_buttons = ['г.Санкт-Петербург, Наставников пр, 3', 'г.Санкт-Петербург, пр-кт Косыгина, 31']
+    start_buttons = [selected_store_names.get(key) for key in selected_store_names]
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
     await message.answer('Пожалуйста, выберите магазин', reply_markup=keyboard)
 
 
-@dp.message_handler(Text(equals='г.Санкт-Петербург, Наставников пр, 3'))
+@dp.message_handler(Text(equals=selected_store_names.get('7215')))
 async def nastavnikov_street(message: types.Message):
-    await message.answer('Ожидайте...')
-    chat_id = message.chat.id
-    await send_data(shop_id='7215', chat_id=chat_id)
+    await send_message(message=message, shop_id='7215')
 
 
-@dp.message_handler(Text(equals='г.Санкт-Петербург, пр-кт Косыгина, 31'))
+@dp.message_handler(Text(equals=selected_store_names.get('28870')))
 async def kosigina_street(message: types.Message):
-    await message.answer('Ожидайте...')
-    chat_id = message.chat.id
-    await send_data(shop_id='28870', chat_id=chat_id)
+    await send_message(message=message, shop_id='28870')
+
+
+@dp.message_handler(Text(equals=selected_store_names.get('7179')))
+async def sadovaya_street(message: types.Message):
+    await send_message(message=message, shop_id='7179')
 
 
 async def send_data(shop_id, chat_id):
     file = await collect_data(shop_id=shop_id)
     await bot.send_document(chat_id=chat_id, document=open(file, 'rb'))
     await os.remove(file)
+
+
+async def send_message(message, shop_id):
+    await message.answer('Ожидайте...')
+    chat_id = message.chat.id
+    await send_data(shop_id=shop_id, chat_id=chat_id)
 
 
 def main():
--- parse.py
+++ parse.py
@@ -1,15 +1,20 @@
 import datetime
+import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-import aiohttp
 import aiofiles
 import asyncio
 from aiocsv import AsyncWriter
 
+selected_store_names = {
+    '28870': 'Косыгина, 31',
+    '7215': 'Наставников пр, 3',
+    '7179': 'Садовая, 69 лит.А'
+}
+
 
 async def collect_data(shop_id):
     cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
-    ua = UserAgent()
 
     selected_store = {
         '28870':
@@ -25,69 +30,60 @@ async def collect_data(shop_id):
             '"address": "г.Санкт-Петербург, Наставников пр, 3",'
             '"work_start_time": "09:00:00",'
             '"work_end_time": "23:00:00",'
+            '"is_24h": false};',
+        '7179':
+            '{"id": 7179, '
+            '"sap_code": "5593", '
+            '"address": "г.Санкт-Петербург, ул. Садовая, 69 лит.А",'
+            '"work_start_time": null, '
+            '"work_end_time": null, '
             '"is_24h": false};'
     }
 
-    selected_store_names = {
-        '28870': 'Косыгина, 31',
-        '7215': 'Наставников пр, 3'
-    }
-
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
-                  'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'User-Agent': ua.random
+        'User-Agent': UserAgent().random
     }
 
     cookies = {
-    #     'selectedStore': f'{selected_store[shop_id]}'
-
-    #     'selectedStore': '%7B%22id%22%3A28870%2C%22sap_code%22%3A%2233YU%22%2C%22address%22%3A%22%D0%B3.%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%BF%D1%80-%D0%BA%D1%82%20%D0%9A%D0%BE%D1%81%D1%8B%D0%B3%D0%B8%D0%BD%D0%B0%2C%2031%22%2C%22work_start_time%22%3A%2208%3A00%3A00%22%2C%22work_end_time%22%3A%2223%3A00%3A00%22%2C%22is_24h%22%3Afalse%7D"'
+            'selectedStore': f'{selected_store[shop_id]}'.encode('utf-8').decode('latin-1')
     }
+    response = requests.get(url='https://5ka.ru/special_offers', headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    async with aiohttp.ClientSession() as session:
+    cards = soup.find_all("div", {'class': 'product-card item'})
 
-        response = await session.get(url='https://5ka.ru/special_offers', headers=headers, cookies=cookies)
-        soup = BeautifulSoup(await response.text(), 'lxml')
+    data = []
+    for card in cards:
+        card_name = card.find('div', {'class': 'item-name'}).get_text(strip=True)
 
-        cards = soup.find_all("div", {'class': 'product-card item'})
+        card_price = card.find('div', {'class': 'price-discount'})
 
-        data = []
-        for card in cards:
-            print(card)
-            card_name = card.find('div', {'class': 'item-name'}).get_text(strip=True)
+        card_discount_int = card_price.find('span').get_text(strip=True)
+        card_discount_dec = card_price.find('div', {'class': 'price-right'})\
+            .find('span', {'class': 'price-discount_cents'}).get_text(strip=True)
+        card_discount_price = f'{card_discount_int}.{card_discount_dec}'
 
-            card_price = card.find('div', {'class': 'prices'}).find('div', {'class': 'price-discount'})
+        card_old_price = card_price.find('div', {'class': 'price-right'})\
+            .find('span', {'class': 'price-regular'}).get_text(strip=True)
 
-            print('\n')
+        card_discount = card.find('div', {'class': 'discount-hint hint'}).get_text(strip=True).replace('-', '')
 
-            print(card_price)
+        card_sale_date = card.find('div', {'class': 'item-date'}).get_text(strip=True)
 
-            card_discount_int = card_price.find('span').get_text(strip=True)
-            card_discount_dec = card_price.find('div', {'class': 'price-right'})\
-                .find('span', {'class': 'price-discount_cents'}).get_text(strip=True)
-            card_price_old_int = card_price.find('div', {'class': 'price-right'})\
-                .find('span', {'class': 'price-regular'}).get_text(strip=True)
-            card_price_old_dec = card_price.find('sup').get_text(strip=True)
+        data.append([card_name, card_discount_price, card_discount, card_old_price, card_sale_date])
 
-            card_old_price = f'{card_price_old_int}.{card_price_old_dec}'
-            card_discount_price = f'{card_discount_int}.{card_discount_dec}'
-            # card_discount = f'{round(int(card_discount_price) / int(card_old_price), 4)  * 100}%'
-
-            card_sale_date = card.find('div', {'class': 'item-date'}).get_text(strip=True)
-
-            data.append([card_name, card_discount_price, card_old_price, card_sale_date])
-
-    async with aiofiles.open(f'{selected_store_names[shop_id]}_{cur_time}.csv', 'w', encoding='utf-8') as file:
+    async with aiofiles.open(f'{selected_store_names[shop_id]}_{cur_time}.csv', 'w',
+                             encoding='utf-8',
+                             newline='') as file:
         writer = AsyncWriter(file)
 
         await writer.writerow(
             [
-                'Product',
-                'New cost',
-                'Old cost',
-                # 'Discount (percent)',
-                'Discount period',
+                'Продукт',
+                'Новая цена',
+                'Скидка',
+                'Старая цена',
+                'Период акции',
             ]
         )
         await writer.writerows(data)
@@ -96,7 +92,7 @@ async def collect_data(shop_id):
 
 
 async def main():
-    await collect_data(shop_id='7215')
+    await collect_data(shop_id='7179')
 
 
 if __name__ == '__main__':
